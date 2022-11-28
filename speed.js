b8f699362241a164075d867cb7bf61252cff2858
Mickey254-ja <mickey.awino@student.moringaschool.com>
2022-11-28 16:04:59 +0300

    introduced if statement

--- speed.js
+++ speed.js
@@ -4,3 +4,12 @@ Speed < 70 ; print OK!
 Speed > 70,deduct 1 point for every 5km.
 Suspend licence on more than 12 points.
 */
+function speedDetector(speed){
+    if (speed<70){
+        console.log("OK")
+    }
+    else {
+        let demeritPoint = ((speed-70)/5)
+        console.log
+    }
+}
