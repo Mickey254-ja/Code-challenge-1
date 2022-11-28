d586935502111bdbfa882a095243b6e6d1bf8292
Mickey254-ja <mickey.awino@student.moringaschool.com>
2022-11-28 15:18:29 +0300

    added grade.js file

---
+++ grade.js
@@ -0,0 +1,21 @@
+//Grade student marks as input by a user
+let grade = (prompt("Enter students marks"))
+
+If (marks <= 100 && marks >79){
+    console.log("A")
+}
+else if (marks <= 79 && marks >=60){
+    console.log("B")
+}
+else if (marks <=59 && marks >= 50){
+    console.log("C")
+}
+else if (marks <=49 && marks >=40){
+    console.log("D")
+}else if (marks <40 && >=0){
+    console.log
+}
+
+
+
+
